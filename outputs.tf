b57ff87fdafddcd8b6eb63021efffff08cdb4597
Matejusz <smetmateusz@gmail.com>
2023-10-03 18:07:21 +0200

    Added output formating

--- outputs.tf
+++ outputs.tf
@@ -3,7 +3,7 @@
 
 # Display the IP for connection
 output "patchwerk_public_ip" {
-  value       = aws_instance.patchwerk_vm.public_ip
+  value       = format("Connect via SSH= ssh msmet@", aws_instance.patchwerk_vm.public_ip)
   sensitive   = false
   description = "Public IP of the VM"
   depends_on  = [aws_instance.patchwerk_vm]
